78d8af951a2b42c34bfb4d244d71bc164fa0248c
moussasg <117439847+moussasg@users.noreply.github.com>
2023-09-21 13:48:52 +0100

    webpackupdate

---
+++ webpack.config.cjs
@@ -0,0 +1,36 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'OrbitControls': path.resolve(__dirname, 'chemin/vers/OrbitControls.js')
+    }
+  }
+};
