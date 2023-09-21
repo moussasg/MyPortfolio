fcf801fe7148a874f1dea9ca503ec1e162590d5b
moussasg <117439847+moussasg@users.noreply.github.com>
2023-09-21 15:44:07 +0100

    modifiedwebpack

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 // webpack : web bundling tool that efficiently combines and processes various source files like JavaScript, CSS, and images
 module.exports = {
   mode: 'development',
-  entry: './public/index.html',
+  entry: '/index.html',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
